40741a25b24c3061d06939a2390a2ee86d394497
Elizabeth <elizabethrk@yandex.ru>
2026-02-26 14:41:56 +0300

    2 лаба

--- lab-2/src/main/java/com/karjakina/labs/db/ConnectionConfig.java
+++ lab-2/src/main/java/com/karjakina/labs/db/ConnectionConfig.java
@@ -57,7 +57,7 @@ public class ConnectionConfig {
 
             liquibase.update();  //
         } finally {
-            connection.close();  // обязательно закрываем соединение
+            connection.close();
         }
     }
     */
